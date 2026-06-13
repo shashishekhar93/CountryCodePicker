069ac1896d96c0f844b3094c4f260ce7886e871d
shashishekhar <p.shekharmishra93@gmail.com>
2026-06-13 19:15:33 +0530

    gradle code updated.

--- countrycodepicker/build.gradle.kts
+++ countrycodepicker/build.gradle.kts
@@ -30,9 +30,6 @@ android {
     publishing {
         singleVariant("release") {
             withSourcesJar()
-
-            // Remove this for JitPack stability
-            // withJavadocJar()
         }
     }
 }
@@ -40,69 +37,10 @@ android {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
-
+            create<MavenPublication>("maven") {
                 groupId = "com.github.shashishekhar93"
                 artifactId = "CountryCodePicker"
                 version = "1.0.0"
-            }
-        }
-    }
-}
-
-dependencies {
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-
-    implementation("io.michaelrocks:libphonenumber-android:9.0.32")
-
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
-/*
-plugins {
-    alias(libs.plugins.android.library)
-    id("maven-publish")
-}
-
-android {
-    namespace = "com.smcoding.countrycodepicker"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    buildFeatures{
-        buildConfig = true
-        viewBinding = true
-    }
-    publishing{
-        singleVariant("release"){
-            withSourcesJar()
-            withJavadocJar()
-        }
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release"){
-                groupId = "com.smcoding"
-                artifactId = "countrycodepicker"
-                version = "1.0.0"
 
                 from(components["release"])
             }
@@ -114,8 +52,9 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
     implementation("io.michaelrocks:libphonenumber-android:9.0.32")
-}*/
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
